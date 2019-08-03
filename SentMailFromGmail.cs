af0708c05c9408f056d940c19d5e3ae85db92f48
karmanjayVerma <karmanjayverma@gmail.com>
2019-08-04 01:48:13 +0530

    Update and rename Form1.cs to SentMailFromGmail.cs

--- SentMailFromGmail.cs
+++ SentMailFromGmail.cs
@@ -3,13 +3,13 @@
                 MailMessage mail = new MailMessage();
                 SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
-                mail.Fro            m = new MailAddress("vermakarmanjay@gmail.com");
-                mail.To.Add("karmanjayverma@gmail.com");
+                mail.Fro            m = new MailAddress("************@gmail.com");
+                mail.To.Add("************@gmail.com");
                 mail.Subject = "Test Mail";
                 mail.Body = "This is for testing SMTP mail from GMAIL";
 
                 SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("vermakarmanjay@gmail.com", "9781829123");
+                SmtpServer.Credentials = new System.Net.NetworkCredential("************@gmail.com", "************");
                 SmtpServer.EnableSsl = true;
 
                 SmtpServer.Send(mail);
